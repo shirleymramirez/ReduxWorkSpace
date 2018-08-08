d4c0427a8c07a889ead170b317308418852e05d3
unknown <shirley.mramirez@yahoo.com>
2018-08-08 15:25:47 -0700

    create post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=DOLLHOUSE1234';
@@ -19,4 +20,14 @@ export function fetchPosts() {
     //resolve that request whenever it sees this action come across
     //so by the time this action arrives at the reducer, payload will contain the response object
     // from axios which will have the big array of posts.
+}
+
+export function createPost(values) {
+    // first argument is the URL, and the second argument is the object or data that we want to send in the remote API
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POST,
+        payload: request
+    };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field; //nested destructuring ES6
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
-                    {...field.imput}
+                    {...field.input}
+                    //this is the same as below: 
+				    // onChange={field.input.onChange}
+				    // onFocus={field.input.onFocus}
+				    // onBlur={field.input.onBlur}
+
                 />
+                <div className="text-help">
+                    {touched ? error : ' '}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        this.props.createPost(values);
+        console.log(values);
+    }
 
     render() {
+        const { handleSubmit } = this.props;
          // Field component is used to represent  a distinct input  that will be visible on screen to our users
          // name property specifies exactly what piece of state this field is going to produce
          // component property is a function that returns some amount of JSX
         return (
-           <form>
+           <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                <Field
                     label="Title for Post"
                     name="title"
@@ -37,6 +55,8 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                />
+               <button type="submit" className="btn btn-primary">Submit</button>
+               <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -46,8 +66,8 @@ function validate(values) {
     const errors = {};
 
     // Validate the inputs from 'values
-    if(!values.title || values.title.length < 3) {
-        errors.title = "Enter a title that is atleast 3 charaters!";
+    if(!values.title) {
+        errors.title = "Enter a title!";
     }
 
     if(!values.categories) {
@@ -68,4 +88,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-}) (PostsNew);
+})(
+    connect(null, { createPost }) (PostsNew)
+);
