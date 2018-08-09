aac21c8e02ea6985045e9b57b4a988375dd2718f
unknown <shirley.mramirez@yahoo.com>
2018-08-08 18:41:17 -0700

    added posts_show component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,7 +17,11 @@ ReactDOM.render(
    <BrowserRouter >
     <div>
       <Switch>
+        {/* we use Route Component here to show the Post's new component */}
+        {/* whenever react router renders a component it passes in a whole new bunch of */}
+        {/* different helpers and objects for helping with navigation to this component */}  
         <Route path="/posts/new" component={PostsNew} />  
+        <Route path="/posts/:id" component={PostsShow} />
         <Route path="/" component={PostsIndex} />
       </Switch>
     </div>
